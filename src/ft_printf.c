e8d341622faeb112f803db747764652bf89c4a1c
Quentin Morineau <142665919+qmorineau@users.noreply.github.com>
2024-10-10 18:19:34 +0200

    test

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: qmorinea <qmorinea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:44:25 by qmorinea          #+#    #+#             */
-/*   Updated: 2024/10/10 16:56:34 by qmorinea         ###   ########.fr       */
+/*   Updated: 2024/10/10 18:18:36 by qmorinea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,44 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+
+
+int ft_count_args(va_arg)
+{
+	int i;
+	
+	i = 0;
+	return (i)
+}
+
 int	ft_printf(const char *a, ...)
 {
-	va_list	arguments;
+	va_list	count_arg;
+	va_list args;
 	int		i;
 
 	i = 0;
-	va_start(arguments, a);
-	while (i < 3)
+	va_start(args, a);
+	va_copy(count_arg, args);
+	i = ft_count_args(count_arg);
+	printf("1st param = '%s'\n", a);
+	while (va_arg(args, char *))
 	{
-		va_arg(arguments, i);
+		printf("%s\n", args);
+		i++;
 	}
-	va_end(arguments);
+	va_end(args);
+	return (1);
 }
 
 int main(void)
 {
-	ft_printf("test", "test1", "test2", "test3");
+	ft_printf("a", "b", "c", "d");
 }
 
 // va_start(va_list ap, parmN)
 // ap = variable that will be initialize by va_start, use to traverse the list of arg
 // parmN = It is name of the last named parameter in the function definition.
+// va_arg(va_list ap, type)
+// type = type of variable
+// va_end
